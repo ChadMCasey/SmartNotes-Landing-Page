5c49b738be5a86efae6cfaf3b69c8e0025249a98
Chad Casey <gbchadd@gmail.com>
2024-05-12 23:16:08 -0700

    add component structure in App & get assets

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,39 @@
 import React from "react";
+
+// css
 import "./App.css";
 
+// components
+import Page from "../Page/Page";
+import Header from "../Header/Header";
+import Hero from "../Hero/Hero";
+import Testimonial from "../Testimonial/Testimonial";
+import Features from "../Features/Features";
+import Video from "../Video/Video";
+import Pricing from "../Pricing/Pricing";
+import FAQ from "../FAQ/FAQ";
+import CTA from "../CTA/CTA";
+import Footer from "../Footer/Footer";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Page>
+      <Header>
+        <Hero />
+      </Header>
+      <Testimonial />
+      <Features />
+      <Testimonial />
+      <Video />
+      <Testimonial />
+      <Pricing />
+      <Testimonial />
+      <FAQ />
+      <Testimonial />
+      <CTA />
+      <Footer />
+    </Page>
+  );
 };
 
 export default App;
